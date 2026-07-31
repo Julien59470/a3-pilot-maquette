5ccbe52d6231c9e3a5b800fa7ffd3c979ce68c6f
Julien59470 <julienfocquenoye@gmail.com>
2026-07-31 19:58:49 +0200

    Ajouter le mode hors connexion

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'a3-pilot-maquette-v4.1';
+const APP_SHELL = [
+  './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon.svg',
+  './chunks/app-01.part', './chunks/app-02.part', './chunks/app-03.part', './chunks/app-04.part', './chunks/app-05.part', './chunks/app-06.part', './chunks/app-07.part', './chunks/styles-01.part', './chunks/styles-02.part', './chunks/styles-03.part', './chunks/styles-04.part'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
