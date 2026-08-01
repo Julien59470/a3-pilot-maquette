921211b5139da0d49c2089ed787f90447bc082cc
Julien59470 <julienfocquenoye@gmail.com>
2026-08-01 19:37:56 +0200

    Renouveler le cache avec les habilitations

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'a3-pilot-maquette-v4.4';
+const CACHE_NAME = 'a3-pilot-maquette-v4.5';
 const APP_SHELL = [
-  './', './index.html', './styles.css', './css/onboarding.css', './app.js', './onboarding.js', './manifest.webmanifest', './assets/icon.svg',
+  './', './index.html', './styles.css', './css/onboarding.css', './css/rbac.css', './app.js', './onboarding.js', './rbac.js', './manifest.webmanifest', './assets/icon.svg',
   './chunks/app-01.part', './chunks/app-02.part', './chunks/app-03.part', './chunks/app-04.part', './chunks/app-05.part', './chunks/app-06.part', './chunks/app-07.part', './css/p01.css', './css/p02.css', './css/p03.css', './css/p04.css', './css/p05.css', './css/p06.css', './css/p07.css', './css/p08.css', './css/p09.css'
 ];
 
