b887811a3e98ffb2b7084a1afcab4b5ce9d5ee37
Julien59470 <julienfocquenoye@gmail.com>
2026-08-02 07:02:24 +0200

    Actualiser le cache des onboardings sans scroll

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'a3-pilot-maquette-v4.6';
+const CACHE_NAME = 'a3-pilot-maquette-v4.7';
 const APP_SHELL = [
   './', './index.html', './styles.css', './css/onboarding.css', './css/rbac.css', './app.js', './onboarding.js', './rbac.js', './manifest.webmanifest', './assets/icon.svg',
   './chunks/app-01.part', './chunks/app-02.part', './chunks/app-03.part', './chunks/app-04.part', './chunks/app-05.part', './chunks/app-06.part', './chunks/app-07.part', './css/p01.css', './css/p02.css', './css/p03.css', './css/p04.css', './css/p05.css', './css/p06.css', './css/p07.css', './css/p08.css', './css/p09.css'
